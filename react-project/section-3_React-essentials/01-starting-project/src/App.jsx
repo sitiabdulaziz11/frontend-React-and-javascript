5577616864d61d3fa9065b2adc1c79db5c9cc829
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-26 15:41:21 +0300

    react app

--- react-project/section-3_React-essentials/01-starting-project/src/App.jsx
+++ react-project/section-3_React-essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentimg from './assets/components.png';
+
 function MyFunc() {
   return "aselamu aleycum"
 }
@@ -10,14 +13,15 @@ function genRandomInt(max) {
 
 function Header() {
   //const description = reactDescriptions[genRandomInt(2)] this more leaner
+  // or {description} React concepts you will need for almost any app you are/ we put this line of code on line 21 if we use this way.
+        
 
   return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom}" />
         <h1>React Essentials</h1>
         <p>
-        {description} React concepts you will need for almost any app you are
-        or
+       
         {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
         </p>
@@ -27,12 +31,33 @@ function Header() {
 // not used in react Header() like this here to call it outside of fun, but in App fun we call like below 1 of the two.
 // we can write <Header></Header> or <Header />
 
+function CoreConcept (props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <MyFunc />
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components"
+            description="The core UI building block."
+            image={componentimg} />
+            <CoreConcept title="props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
