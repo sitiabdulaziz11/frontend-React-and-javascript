5ac753235f417bb8d451ff0ee57a1bf3daa0142c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-27 15:01:48 +0300

    the starting of new game project creation

---
+++ react-project/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+
+  //return <p>Coming soon...</p>;
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbole">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbole">O</span>
+        </li>
+
+      </ol>
+    </div>
+  </main>
+
+  
+}
+    {/* return (
+      inserted in index.html file above <div id="root"></div>
+      <header>
+      <img src="" alt="" />
+      <h1>React Tic-Tac-Toe</h1>
+    </header>
+    )*/}
+
+export default App
