2f9982aadc42bc8fefec6b340a73e894d281b4f4
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-06-19 13:59:27 +0300

    last updated

--- react-project/04 Essentials Deep Dive/01-starting-project/src/components/try/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Tabs({children, buttons, buttonContainer }) {
-    const ButtonContainer = buttonContainer; //this should start with uppercase character  = buttonsContainer;
-    return <>
-    <ButtonContainer>
-        {buttons}
-    </ButtonContainer>
-    {children}
-    </>
-}
