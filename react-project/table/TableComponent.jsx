3055e592a66e36d4a1550f8764f4d28df733f181
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-06-24 10:56:14 +0300

    sample table

---
+++ react-project/table/TableComponent.jsx
@@ -0,0 +1,28 @@
+// src/TableComponent.js
+import React from 'react';
+import './TableComponent.css';
+
+const TableComponent = ({ data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(data[0]).map((key) => (
+            <th key={key}>{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {Object.values(row).map((value, i) => (
+              <td key={i}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableComponent;
