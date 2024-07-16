5f12b068a4336f7d2707b3691dc6b21c239572e9
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-07-16 20:07:13 +0300

    tailwind css

--- react-project/06-styling react component/tailwind.config.js
+++ react-project/06-styling react component/tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"pacifico"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
-
