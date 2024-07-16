1b644ef0c47390cc7a838b8fe082ec0b5929ad43
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-07-16 18:08:06 +0300

    tailwind css

--- react-project/06-styling react component/src/components/Header.jsx
+++ react-project/06-styling react component/src/components/Header.jsx
@@ -1,14 +1,11 @@
 import logo from '../assets/logo.png';
-import classes from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
-      {/* <p className={1== 1 ? classes.paragraph : undefined}>A community of artists and art-lovers.</p> or */}
-      {/* <p className={`${classes.paragraph}`}>A community of artists and art-lovers.</p> */}
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className='object-contain mb-8 w-22 h-22' />
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p >A community of artists and art-lovers.</p>
     </header>
   );
 }
