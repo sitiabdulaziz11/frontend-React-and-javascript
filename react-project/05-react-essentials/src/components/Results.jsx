d8fe105350d5a8864b64a54cb8fec1cffecb8593
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-07-21 23:14:58 +0300

    updated

--- react-project/05-react-essentials/src/components/Results.jsx
+++ react-project/05-react-essentials/src/components/Results.jsx
@@ -1,40 +1,91 @@
-// import { calculateInvestmentResults, formatter } from "../util/investment.js";
+// // import { calculateInvestmentResults, formatter } from "../util/investment.js";
+
+// // export default function Results({ input }) {
+// //     const resultsData = calculateInvestmentResults(input);
+// //     const initialInvestment =
+// //       resultsData[0].valueEndOfYear -
+// //       resultsData[0].interest -
+// //       resultsData[0].annualInvestment;
+
+// //     return (
+// //         <table id='results'>
+// //             <thead>
+// //                 <tr>
+// //                    <th>Year</th> 
+// //                    <th>Investment Value</th>
+// //                    <th>Profit (Year)</th>
+// //                    <th>Total Profit</th>
+// //                    <th>Invested Capital</th>
+// //                 </tr>
+// //             </thead>
+// //             <tbody>
+// //                 {resultsData.map(yearData => {
+// //                     const totalProfit = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+// //                     const totalAmountInvested = yearData.valueEndOfYear - totalProfit;
+
+// //                     return <tr key={yearData.year}>
+// //                         <td>{yearData.year}</td>
+// //                         <td>{formatter.format(yearData.valueEndOfYear)}</td>
+// //                         <td>{formatter.format(yearData.profit)}</td>
+// //                         <td>{formatter.format(totalProfit)}</td>
+// //                         <td>{formatter.format(totalAmountInvested)}</td>
+// //                     </tr>
+// //                 })}
+// //             </tbody>
+// //         </table>
+// //     )
+// // }
+
+// import { calculateInvestmentResults, formatter } from '../util/investment.js';
+
 
 // export default function Results({ input }) {
-//     const resultsData = calculateInvestmentResults(input);
-//     const initialInvestment =
-//       resultsData[0].valueEndOfYear -
-//       resultsData[0].interest -
-//       resultsData[0].annualInvestment;
+//   const results = [];
+//   calculateInvestmentResults(input, results);
+  
+//   if (results.length === 0) {
+//     return <p className='center'>Invalid input data provided.</p>
+//   }
+  
+//   const initialInvestment =
+//     results[0].valueEndOfYear -
+//     results[0].interest -
+//     results[0].annualInvestment;
 
-//     return (
-//         <table id='results'>
-//             <thead>
-//                 <tr>
-//                    <th>Year</th> 
-//                    <th>Investment Value</th>
-//                    <th>Profit (Year)</th>
-//                    <th>Total Profit</th>
-//                    <th>Invested Capital</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {resultsData.map(yearData => {
-//                     const totalProfit = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
-//                     const totalAmountInvested = yearData.valueEndOfYear - totalProfit;
+//   return (
+//     <table id="result">
+//       <thead>
+//         <tr>
+//           <th>Year</th>
+//           <th>Investment Value</th>
+//           <th>Interest (Year)</th>
+//           <th>Total Interest</th>
+//           <th>Invested Capital</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {results.map((yearData) => {
+//           const totalInterest =
+//             yearData.valueEndOfYear -
+//             yearData.annualInvestment * yearData.year -
+//             initialInvestment;
+//           const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
 
-//                     return <tr key={yearData.year}>
-//                         <td>{yearData.year}</td>
-//                         <td>{formatter.format(yearData.valueEndOfYear)}</td>
-//                         <td>{formatter.format(yearData.profit)}</td>
-//                         <td>{formatter.format(totalProfit)}</td>
-//                         <td>{formatter.format(totalAmountInvested)}</td>
-//                     </tr>
-//                 })}
-//             </tbody>
-//         </table>
-//     )
+//           return (
+//             <tr key={yearData.year}>
+//               <td>{yearData.year}</td>
+//               <td>{formatter.format(yearData.valueEndOfYear)}</td>
+//               <td>{formatter.format(yearData.interest)}</td>
+//               <td>{formatter.format(totalInterest)}</td>
+//               <td>{formatter.format(totalAmountInvested)}</td>
+//             </tr>
+//           );
+//         })}
+//       </tbody>
+//     </table>
+//   );
 // }
+
 
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
@@ -42,11 +93,11 @@ import { calculateInvestmentResults, formatter } from '../util/investment.js';
 export default function Results({ input }) {
   const results = [];
   calculateInvestmentResults(input, results);
-  
+
   if (results.length === 0) {
-    return <p className='center'>Invalid input data provided.</p>
+    return <p className="center">Invalid input data provided.</p>
   }
-  
+
   const initialInvestment =
     results[0].valueEndOfYear -
     results[0].interest -
