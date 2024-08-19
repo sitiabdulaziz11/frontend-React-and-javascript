ecaf9179bc1b644b477b84c0d08b6e3fc16d089d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-08-19 22:31:29 +0300

    side bar

--- react-project/09 Practice Project - Project Management/src/App.jsx
+++ react-project/09 Practice Project - Project Management/src/App.jsx
@@ -1,11 +1,35 @@
-import NewProject from "./components/NewProject";
-import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // undefinded means doing no thing.
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // to add new project
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar />
-      <NewProject /> 
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject}/> */}
+      {/* <NewProject />  */}
     </main>
   );
 }
