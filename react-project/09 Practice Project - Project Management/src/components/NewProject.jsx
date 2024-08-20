46222363fe36eca197531d161ce7a79a6095f00c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-08-20 17:54:22 +0300

    titel

--- react-project/09 Practice Project - Project Management/src/components/NewProject.jsx
+++ react-project/09 Practice Project - Project Management/src/components/NewProject.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import Input from './Input.jsx'
+import {useRef} from 'react';
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate= dueDate.current.value;
+
+    // validation...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className='flex items-center justify-end gap-4 my-4'>
         <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-        <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
+        <li><button
+        className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950' onClick={handleSave}>
+          
+          Save
+          </button></li>
       </menu>
       <div>
-
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due date" />
         {/* <p>
          <label>Title</label>
          <input />    
@@ -22,9 +47,7 @@ const NewProject = () => {
          <label>Due Date</label>
          <input />    
         </p> */}
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due date" />
+        
       </div>
     </div>
   )
