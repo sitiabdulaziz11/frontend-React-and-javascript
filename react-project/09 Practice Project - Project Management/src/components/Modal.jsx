53568e3c7bc3b6a13ad12a01f6297658d5648d64
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-08-25 21:33:20 +0300

    cancel button

--- react-project/09 Practice Project - Project Management/src/components/Modal.jsx
+++ react-project/09 Practice Project - Project Management/src/components/Modal.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {forwardRef, useImperativeHandle, useRef} from 'react';
-import {createPortal} from 'react-dom';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import Button from './Button.jsx'
 
-const Modal = forwardRef(({children, buttonCaption}, ref) => {
+const Modal = forwardRef(({ children, buttonCaption }, ref) => {
     const dialog = useRef();
     useImperativeHandle(ref, () => {
         return {
@@ -12,14 +13,14 @@ const Modal = forwardRef(({children, buttonCaption}, ref) => {
         };
     });
 
-  return createPortal(
-    <dialog ref={dialog}>
-        {children}
-        <form method="dialog">
-            <button>{buttonCaption}</button>
-        </form>
-    </dialog>, document.getElementById('modal-root')
-  );
+    return createPortal(
+        <dialog ref={dialog} className='backdrop:bg-stone-900/90 p-4 rounded-md shadow-md'>
+            {children}
+            <form method="dialog" className='mt-4 text-right'>
+                <Button>{buttonCaption}</Button>
+            </form>
+        </dialog>, document.getElementById('modal-root')
+    );
 });
 
 export default Modal
--- react-project/09 Practice Project - Project Management/src/components/NewProject.jsx
+++ react-project/09 Practice Project - Project Management/src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import Input from './Input.jsx'
 import Modal from './Modal.jsx';
 import { useRef } from 'react';
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd, onCancel }) => {
   const modal = useRef();
 
   const title = useRef();
@@ -31,14 +31,17 @@ const NewProject = ({ onAdd }) => {
 
   return (
     <>
-    <Modal ref={modal} buttonCaption="Okay">
-      <h2>Invalid Input</h2>
-      <p>Oops ... lools like you forgot to enter a value.</p>
-      <p>Please make shur you provide a valid value for every input field.</p>
-    </Modal>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+        <p className='text-stone-600  mb-4'>Oops ... lools like you forgot to enter a value.</p>
+        <p className='text-stone-600  mb-4'>Please make shur you provide a valid value for every input field.</p>
+      </Modal>
       <div className="w-[35rem] mt-16">
         <menu className='flex items-center justify-end gap-4 my-4'>
-          <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
+          <li>
+            <button className='text-stone-800 hover:text-stone-950' onClick={onCancel}>Cancel
+            </button>
+            </li>
           <li><button
             className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950' onClick={handleSave}>
 
