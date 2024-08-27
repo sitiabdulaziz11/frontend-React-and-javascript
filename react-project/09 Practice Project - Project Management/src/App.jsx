e1a385f9b62730d3e13ab7e7f4a74941e8655b93
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-08-27 22:32:37 +0300

    Delete button

--- react-project/09 Practice Project - Project Management/src/App.jsx
+++ react-project/09 Practice Project - Project Management/src/App.jsx
@@ -60,9 +60,21 @@ function App() {
 
   // console.log(projectsState);
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined, // to add new project
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId); // used for finding an element in an array by ID.
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+  <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
