d5fb2fb465dd8df87dd80975700313390fb30bed
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-09-01 23:34:51 +0300

    add tasks

--- react-project/09 Practice Project - Project Management/src/components/NewTask.jsx
+++ react-project/09 Practice Project - Project Management/src/components/NewTask.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
-const NewTask = () => {
+const NewTask = ({onAdd}) => {
+  // useRef();
+  const [enterdTask, setEnterdTask] = useState();
+
+  function handleChange(event) {
+    setEnterdTask(event.target.value);
+  }
+
+  function handleClick() {
+    onAdd(enterdTask);
+    setEnterdTask('');
+  }
+
   return (
     <div className='flex items-center gap-4'>
-      <input type='text' className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
-      <button className='text-stone-700 hover:text-stone-950'>Add Tasks</button>
+      <input type='text'
+      className='w-64 px-2 py-1 rounded-sm bg-stone-200'
+      onChange={handleChange}
+      value={enterdTask}
+      />
+      <button className='text-stone-700 hover:text-stone-950'
+      onClick={handleClick}
+      >Add Tasks</button>
     </div>
   )
 }
--- react-project/09 Practice Project - Project Management/src/components/Tasks.jsx
+++ react-project/09 Practice Project - Project Management/src/components/Tasks.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import NewTask from './NewTask.jsx'
 
-const Tasks = () => {
+const Tasks = ({ tasks, onAdd, onDelete }) => {
   return (
     <section>
-        <h2 className='text-2xl font-bold text-stone-700 mb-4'></h2>
-        {/* NEW TASK */}
-        <NewTask />
-        <p className='text-stone-800 my-4'>This projects dose not have any tasks yet.</p>
-        <ul></ul>
+      <h2 className='text-2xl font-bold text-stone-700 mb-4'></h2>
+      {/* NEW TASK */}
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && { onAdd }(
+        <p className='text-stone-800 my-4'>This projects dose not have any tasks yet.
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul className='p-4 mt-8 rounded-md bg-stone-100'>
+          {tasks.map((task) => (
+            <li key={task.id} className='flex justify-between my-4'>
+              <span>{task.text}</span>
+              <button className='text-stone-700 hover:text-red-500'>Clear</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   )
 }
