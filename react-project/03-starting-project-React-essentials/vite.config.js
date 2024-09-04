7545bf4bd58b1b0e563982b1e5dd4f8f07de5117
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-09-04 19:12:57 +0300

    updated

--- react-project/03-starting-project-React-essentials/vite.config.js
+++ react-project/03-starting-project-React-essentials/vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1/': {
+        target: 'http://0.0.0.0:5000',  // Replace with your Flask backend's address
+        changeOrigin: true,
+      },
+    },
+  },
 });
