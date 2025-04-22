df4113ddf0adc502d2e8fc117c6b79dfdd55f17d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-22 15:45:26 +0300

    updated

--- feedback/review/models.py
+++ feedback/review/models.py
@@ -8,4 +8,4 @@ class Review(models.Model):
     user_name = models.CharField(max_length=100)  # max_length is for counfiguring database setup not only for validation 
     review_text = models.TextField()
     rating = models.IntegerField()
-    
+    # owner_comment = models.TextField()
