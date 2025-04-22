0ea2893d1bac20f2045faaf222b28b86758bc395
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-22 13:31:38 +0300

    django db and forms

--- feedback/review/models.py
+++ feedback/review/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    """Review model. model is for storing data not fetching.
+    """
+    user_name = models.CharField(max_length=100)  # max_length is for counfiguring database setup not only for validation 
+    review_text = models.TextField()
+    rating = models.IntegerField()
+    
