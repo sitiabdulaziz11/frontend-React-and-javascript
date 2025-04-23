347d7b4c58d3340b8377dba6d0688db2f310bcf3
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-23 14:56:31 +0300

    class based views

--- feedback/review/views.py
+++ feedback/review/views.py
@@ -18,52 +18,60 @@ class ReviewView(View):
         """
         form = ReviewForms()
         return render(request, 'reviews/review.html', {
-            "form": form
+            "form1": form
         })
         
     def post(self, request):
         """
         function to handle post request
         """
-    
-
-def review(request):
-    """
-    Function to handle review requests.
-    """
-    if request.method == 'POST':
-        # enterd_username = request.POST['username']  # request here is to reade some dtata from the request, POST to accecce the data it self.
-        
-        # if enterd_username == "" and len(enterd_username) >= 100:
-        #     return render(request, 'reviews/review.html', {
-        #             "has_error": True
-        #         })
         form = ReviewForms(request.POST)
         
-        # existing_data = Review.objects.get(pk=1)  # this is to retrive data from database
-        # form = ReviewForms(request.POST, instance=existing_data) # this two line to update data if we need
-        
         if form.is_valid():
-        #    print(form.cleaned_data)   
-        #    review = Review(
-        #        user_name=form.cleaned_data['user_name'],
-        #        review_text=form.cleaned_data['review_text'],
-        #        rating=form.cleaned_data['rating'])
-        #    review.save()
-           form.save()
-           return HttpResponseRedirect('/thank-you')
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+            "form1": form
+        })
+
+# def review(request):
+#     """
+#     Function to handle review requests.
+#     """
+#     if request.method == 'POST':
+#         # enterd_username = request.POST['username']  # request here is to reade some dtata from the request, POST to accecce the data it self.
+        
+#         # if enterd_username == "" and len(enterd_username) >= 100:
+#         #     return render(request, 'reviews/review.html', {
+#         #             "has_error": True
+#         #         })
+#         form = ReviewForms(request.POST)
+        
+#         # existing_data = Review.objects.get(pk=1)  # this is to retrive data from database
+#         # form = ReviewForms(request.POST, instance=existing_data) # this two line to update data if we need
+        
+#         if form.is_valid():
+#         #    print(form.cleaned_data)   
+#         #    review = Review(
+#         #        user_name=form.cleaned_data['user_name'],
+#         #        review_text=form.cleaned_data['review_text'],
+#         #        rating=form.cleaned_data['rating'])
+#         #    review.save()
+#            form.save()
+#            return HttpResponseRedirect('/thank-you')
            
-        # print(enterd_username)
-        # return render(request, "reviews/thank_you.html")
-        # return HttpResponseRedirect('/thank-you')
+#         # print(enterd_username)
+#         # return render(request, "reviews/thank_you.html")
+#         # return HttpResponseRedirect('/thank-you')
     
-    else:
-        form = ReviewForms()
+#     else:      # this is for get method
+#         form = ReviewForms()
     
-    return render(request, 'reviews/review.html', {
-        # "has_error": False
-        "form1": form
-    })
+#     return render(request, 'reviews/review.html', {
+#         # "has_error": False
+#         "form1": form
+#     })
 
 
 def thank_you(request):
