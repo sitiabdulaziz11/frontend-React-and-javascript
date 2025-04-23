246e6966156657c00536f5fb167796eda533dfa1
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-23 14:56:31 +0300

    class based views

--- feedback/review/urls1.py
+++ feedback/review/urls1.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.review),
+    # path("", views.review),
+    path("", views.ReviewView.as_view()),
     path("thank-you", views.thank_you),  # This is the URL pattern for the thank you page.
 ]
