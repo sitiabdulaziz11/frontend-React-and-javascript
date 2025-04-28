a1e664f7a0875f23c1a33917738ab4cbe94c39fe
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-28 13:59:21 +0300

    django project

--- feedback/review/urls1.py
+++ feedback/review/urls1.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path("", views.review),
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you),  # This is the URL pattern for the thank you page.
+    # path("thank-you", views.thank_you),  # This is the URL pattern for the thank you page.
+    path("thank-you", views.ThankYouViews.as_view())
 ]
