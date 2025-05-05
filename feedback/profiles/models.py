9cf0748d3eb692d5ec2523161115e8b7e1c23824
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-05-05 10:20:31 +0300

    django file

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    """To stor file storage
+    """
+    file_image = models.FileField(upload_to="images")   # file is not stord on db, but it's path is stord
+    
