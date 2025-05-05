c95ad27e1ee5e57d2a609745c91a00ec5d8fd9cd
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-05-05 10:20:31 +0300

    django file

---
+++ feedback/profiles/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+class ProfileForm(forms.Form):
+    """To upload file
+    """
+    # user_image = forms.FileField(allo)  # to allow empty file
+    user_image = forms.FileField()
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 
+# def stor_file(file):
+#     """ Store the file in our system.
+#     """
+#     with open("temp/image.png", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+
 class CreateProfileView(View):
+    """To create file
+    """
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        # request.POST  # is only for non-file data
+        if submitted_form.is_valid():
+            # stor_file(request.FILES['image']) # is  for any submitted file data
+            profile = UserProfile(file_image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
