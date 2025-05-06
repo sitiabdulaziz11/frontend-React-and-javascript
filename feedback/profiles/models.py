3c9c6a7a48c1dc78e75072cd16daeb3cf29ffad4
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-05-06 15:14:25 +0300

    django file upload

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class UserProfile(models.Model):
     """To stor file storage
     """
-    file_image = models.FileField(upload_to="images")   # file is not stord on db, but it's path is stord
-    
+    # file_image = models.FileField(upload_to="images")   # file is not stord on db, but it's path is stord, FileField() is used to stor files like pdf..
+    file_image = models.ImageField(upload_to="images")  # to stor only image
